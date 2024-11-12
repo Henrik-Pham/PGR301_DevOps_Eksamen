c961fa94ac5b5b712fcac88429094d1f596c3ce1
EC2 Default User <ec2-user@ip-172-31-41-138.eu-west-1.compute.internal>
2024-11-12 19:27:07 +0000

    fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,11 +5,6 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "bucket_name" {
-  description = "S3 bucket name for storing images"
-  default     = "pgr301-couch-explorers"
-}
-
 variable "prefix" {
   description = "Prefix for naming resources"
   default     = "ka37"
