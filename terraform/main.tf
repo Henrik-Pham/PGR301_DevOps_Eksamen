9d8cee2988d12162d8dc79b8561c9cca70a53dbe
EC2 Default User <ec2-user@ip-172-31-41-138.eu-west-1.compute.internal>
2024-11-17 14:12:00 +0000

    Github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 # Configure Terraform to use an S3 backend for state storage
 terraform {
   backend "s3" {
-    bucket = "pgr301-couch-explorers"         # Using your existing bucket
-    key    = "devops-exam/terraform.tfstate"   # Path for the Terraform state file within the bucket
-    region = "eu-west-1"                       # Ensure this matches your AWS region
+    bucket = "pgr301-couch-explorers"        # Using your existing bucket
+    key    = "devops-exam/terraform.tfstate" # Path for the Terraform state file within the bucket
+    region = "eu-west-1"                     # Ensure this matches your AWS region
   }
 }
 
@@ -31,19 +31,19 @@ resource "aws_iam_role" "lambda_exec_role" {
 
 # IAM policy to allow access to S3 and invoking the Bedrock model
 resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "${var.prefix}_lambda_policy"
-  role   = aws_iam_role.lambda_exec_role.id
+  name = "${var.prefix}_lambda_policy"
+  role = aws_iam_role.lambda_exec_role.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = ["s3:PutObject", "s3:GetObject"],
-        Resource = "arn:aws:s3:::pgr301-couch-explorers/*"  # Permissions for the entire bucket
+        Effect   = "Allow",
+        Action   = ["s3:PutObject", "s3:GetObject"],
+        Resource = "arn:aws:s3:::pgr301-couch-explorers/*" # Permissions for the entire bucket
       },
       {
-        Effect = "Allow",
-        Action = "bedrock:InvokeModel",
+        Effect   = "Allow",
+        Action   = "bedrock:InvokeModel",
         Resource = "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-image-generator-v1"
       }
     ]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,10 @@
 
 variable "aws_region" {
   description = "AWS region to deploy resources in"
-  default     = "eu-west-1"  # Make sure this is the correct region
+  default     = "eu-west-1" # Make sure this is the correct region
 }
 
 variable "prefix" {
   description = "A unique prefix for resource names"
-  default     = "ka37"       # Use your candidate number or unique identifier
+  default     = "ka37" # Use your candidate number or unique identifier
 }
